ce8028a2125c2b23eb8344d2780202a883c33f4c
Adilet <877555666zzz@gmail.com>
2026-09-21 12:14:02 +0500

    mobile: mark the settings push as unawaited

--- mobile/lib/features/profile/my_profile_screen.dart
+++ mobile/lib/features/profile/my_profile_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -36,7 +38,7 @@ class MyProfileScreen extends ConsumerWidget {
     if (action == null || !context.mounted) return;
 
     if (action == 'settings') {
-      context.push(AppRoutes.settings);
+      unawaited(context.push(AppRoutes.settings));
       return;
     }
 
